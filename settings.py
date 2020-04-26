be64d2ade4709ec60a89649b8f826036bb99e901
hirotta <nekopanda_dev@yahoo.co.jp>
2020-04-26 13:35:03 +0900

    first commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+TENANT_ID = os.environ.get("TENANT_ID")
+TARGET_DOMAIN = os.environ.get("TARGET_DOMAIN")
